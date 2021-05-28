108cd6958c129ae068766d0b687da77f80f10050
prograsshopper <tjalwled12@gmail.com>
2021-05-29 03:04:13 +0900

    Typescript: ch2-2

--- Typescript/book/ch2-2/src/index.ts
+++ Typescript/book/ch2-2/src/index.ts
@@ -1,10 +1,9 @@
 import Person, {makePerson} from './utils/makePerson'
 import IPerson from './IPerson'
+import Chance from 'chance'
+import * as R from 'ramda'
 
-const testMakePerson = (): void => {
-    let jane: IPerson = makePerson('Jane')
-    let jack: IPerson = makePerson('Jack')
-    console.log(jane, jack)
-}
-
-testMakePerson()
+const chance = new Chance()
+let persons: IPerson[] = R.range(0, 2)
+    .map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
